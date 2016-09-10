568939e987896e3ee4855d06096ec390fa5db05c
Michael Milette <michael.milette@tngconsulting.ca>
2016-09-10 17:15:07 -0400

    Create core_renderer.php

---
+++ renderers/core_renderer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the wet-boew-moodle theme.
+ *
+ * Copyright © 2016 onwards by TNG Consulting Inc. Inc. 
+ *
+ * WET-BOEW-MOODLE is provided freely as open source software, can be redistributed and/or 
+ * modified it under the terms of the GNU General Public License version 3.0 or later.
+ *
+ * This software is distributed in the hope that it will be useful. However, there is no warranty,
+ * implied or otherwise, of merchantability or fitness for any purpose.
+ *
+ * If for any reason a copy of the GNU General Public License was not included with this project,
+ * you can view it online by going to: https://www.gnu.org/licenses/gpl-3.0.en.html</p>
+*/
+
+/**
+ * Renderers to align Moodle's HTML with that expected by Bootstrap
+ *
+ * @package      theme_wet-boew-moodle
+ * @copyright    2016 TNG Consulting Inc. unless otherwise noted.
+ * @author       Michael Milette <www.tngconsulting.ca>
+ * @package      theme_wet-boew-moodle
+ */
+ 
+defined('MOODLE_INTERNAL') || die();
